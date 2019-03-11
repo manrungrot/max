99f262602ccdd238f0c0042f17fd531331dcf9ef
S҉E҉L҉F҉B҉O҉T҉-҉B҉Y҉:҉M҉A҉X҉ <39564544+0954517662@users.noreply.github.com>
2019-03-11 11:40:28 +0700

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -12,7 +12,6 @@ from bs4 import BeautifulSoup
 from humanfriendly import format_timespan, format_size, format_number, format_length
 import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib.request, urllib.parse, urllib.error, urllib.parse,antolib,subprocess,unicodedata,GACSender
 _session = requests.session()
-from gtts import gTTS
 from googletrans import Translator
 #==============================================================================#
 botStart = time.time()
